94959b33f445c90ab1e812763b6c8be6785b0ef1
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2019-10-21 09:41:00 +0100

    added a new set of decision rules

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const button = document.querySelector(".get-joke-btn");
-const joke = document.querySelector(".joke");
+const button = document.querySelector('.get-joke-btn');
+const displayJoke = document.querySelector('.joke');
 
 const getJoke = event => {
-   fetch("https://sv443.net/jokeapi/category/Programming")
-      .then(response => response.json())
-      .then(joke => {
-         if (joke.type === "single") {
-            joke.innerHTML = `
-            <ul>
-               <li>${joke.joke}</li>
+	fetch('https://sv443.net/jokeapi/category/Programming')
+		.then(response => response.json())
+		.then(joke => {
+			if (joke.type === 'single') {
+				displayJoke.innerHTML = `
+            <ul class="list">
+               <li><h1>${joke.joke}</h1></li>
             </ul>
             `;
-            console.log(joke)
+			} else if (joke.type == 'twopart') {
+				displayJoke.innerHTML = `
+            <ul class="list">
+               <li><h1>Setup: ${joke.setup}</h1></li>
+               <li><h1>Delivery: ${joke.delivery}</h1></li>
+            </ul>
+            `;
+			} else {
+				displayJoke.inner = '<h2>something went wrong, please try again</h2>';
+			}
+		});
+};
 
-         } else {
-            joke.innerHTML = `something went wrong, try again`;
-         }
-      })
-}
-
-
-button.addEventListener("click", getJoke);
+button.addEventListener('click', getJoke);
